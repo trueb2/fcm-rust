28f10100c521ab988d4b8a9095af58435c9c7081
Jasper Visser <jasper.visser@keylane.com>
2020-12-30 21:05:09 +0100

    Removed unnecessary impl future return type

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -30,7 +30,7 @@ impl Client {
     }
 
     /// Try sending a `Message` to FCM.
-    pub fn send(&self, message: Message<'_>) -> impl Future<Output = Result<FcmResponse, FcmError>> + 'static {
+    pub fn send(&self, message: Message<'_>) -> Result<FcmResponse, FcmError> {
         let payload = serde_json::to_vec(&message.body).unwrap();
 
         let builder = Request::builder()
@@ -46,55 +46,53 @@ impl Client {
         let request = builder.body(Body::from(payload)).unwrap();
         let requesting = self.http_client.request(request);
 
-        async move {
-            let response = requesting.await?;
-            let response_status = response.status();
+        let response = requesting.await?;
+        let response_status = response.status();
 
-            let retry_after = response
-                .headers()
-                .get(RETRY_AFTER)
-                .and_then(|ra| ra.to_str().ok())
-                .and_then(|ra| RetryAfter::from_str(ra));
+        let retry_after = response
+            .headers()
+            .get(RETRY_AFTER)
+            .and_then(|ra| ra.to_str().ok())
+            .and_then(|ra| RetryAfter::from_str(ra));
 
-            let content_length: usize = response
-                .headers()
-                .get(CONTENT_LENGTH)
-                .and_then(|s| s.to_str().ok())
-                .and_then(|s| s.parse().ok())
-                .unwrap_or(0);
+        let content_length: usize = response
+            .headers()
+            .get(CONTENT_LENGTH)
+            .and_then(|s| s.to_str().ok())
+            .and_then(|s| s.parse().ok())
+            .unwrap_or(0);
 
-            let mut body: Vec<u8> = Vec::with_capacity(content_length);
-            let mut chunks = response.into_body();
+        let mut body: Vec<u8> = Vec::with_capacity(content_length);
+        let mut chunks = response.into_body();
 
-            while let Some(chunk) = chunks.next().await {
-                body.extend_from_slice(&chunk?);
-            }
+        while let Some(chunk) = chunks.next().await {
+            body.extend_from_slice(&chunk?);
+        }
 
-            match response_status {
-                StatusCode::OK => {
-                    let fcm_response: FcmResponse = serde_json::from_slice(&body).unwrap();
+        match response_status {
+            StatusCode::OK => {
+                let fcm_response: FcmResponse = serde_json::from_slice(&body).unwrap();
 
-                    match fcm_response.error {
-                        Some(ErrorReason::Unavailable) => {
-                            Err(response::FcmError::ServerError(retry_after))
-                        }
-                        Some(ErrorReason::InternalServerError) => {
-                            Err(response::FcmError::ServerError(retry_after))
-                        }
-                        _ => Ok(fcm_response),
+                match fcm_response.error {
+                    Some(ErrorReason::Unavailable) => {
+                        Err(response::FcmError::ServerError(retry_after))
                     }
+                    Some(ErrorReason::InternalServerError) => {
+                        Err(response::FcmError::ServerError(retry_after))
+                    }
+                    _ => Ok(fcm_response),
                 }
-                StatusCode::UNAUTHORIZED => Err(response::FcmError::Unauthorized),
-                StatusCode::BAD_REQUEST => Err(response::FcmError::InvalidMessage(
-                    "Bad Request".to_string(),
-                )),
-                status if status.is_server_error() => {
-                    Err(response::FcmError::ServerError(retry_after))
-                }
-                _ => Err(response::FcmError::InvalidMessage(
-                    "Unknown Error".to_string(),
-                )),
             }
+            StatusCode::UNAUTHORIZED => Err(response::FcmError::Unauthorized),
+            StatusCode::BAD_REQUEST => Err(response::FcmError::InvalidMessage(
+                "Bad Request".to_string(),
+            )),
+            status if status.is_server_error() => {
+                Err(response::FcmError::ServerError(retry_after))
+            }
+            _ => Err(response::FcmError::InvalidMessage(
+                "Unknown Error".to_string(),
+            )),
         }
     }
 }
